eb0a25d60fb3772fd8b9b9b5db6362409be98ba7
Ilya <ilya.guchick07@mail.ru>
2024-12-10 10:35:29 +0300

    newStringsAndNumbers

---
+++ 01-numbers.js
@@ -0,0 +1,65 @@
+/* <------- Reserved for learn   ------>*/
+
+// const num = 20
+// const float = 20.12
+// const pow = 10e3
+// const big = 2_000_000
+
+// console.log(big)
+
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.MIN_SAFE_INTEGER)
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.NEGATIVE_INFINITY)
+// console.log(Number.POSITIVE_INFINITY)
+// console.log(Number.NaN)
+// // console.log(Number.ifFinite(Infinity))
+// const weird = 23 / undefined
+// console.log(Number.isNaN(weird))
+// console.log(Number.isNaN(23))
+
+const strInt = '21'
+const strFloat = '21.12'
+
+// console.log(Number(strInt))    first method
+// console.log(Number(strFloat))  first method
+
+// console.log(parseInt(strInt)) // second method
+// console.log(parseFloat(strFloat)) // second method
+
+// console.log(+strInt, +strFloat) // third method
+
+// console.log(parseFloat((0.1 + 0.2).toFixed(4)))  //0.30000000 or 0.3
+
+// console.log(BigInt(Number.MAX_SAFE_INTEGER) + 21232323n) // bigInt работает только с целым числом и в конце n
+// console.log(BigInt(-32n))
+// console.log(parseInt(32n) - 22)
+// console.log(32n - BigInt(22))
+
+// console.log(+(13 / 3).toFixed(2))
+// console.log(13n / 3n)
+
+//<---------Match---------->
+// const number = [32, 12, 122, 11, 33]
+
+const myNum = 2.4
+
+// console.log(+Math.sqrt(22).toFixed(2))
+// console.log(Math.pow(2, 3))
+// console.log(Math.abs(-32))
+// console.log(Math.max(2, 32, 199, 212, 3))
+// console.log(Math.min(2, 32, 199, 212, 3))
+
+// console.log(Math.floor(myNum))
+// console.log(Math.ceil(myNum))
+// console.log(Math.round(myNum))
+// console.log(Math.trunc(myNum))
+// console.log(Math.random())
+
+// const getRandomNumber = (min, max) =>
+// 	Math.floor(Math.random() * (max - min + 1) + min)
+
+// const num1 = getRandomNumber(10, 100)
+
+// console.log(num1)
---
+++ 02-strings.js
@@ -0,0 +1,3 @@
+const name = 'Ilya'
+const age = 20
+//4.11.53
