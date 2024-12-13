d7461650fbc1104bf860482bab7e77e07c09b4f4
Ilya <ilya.guchick07@mail.ru>
2024-12-13 19:15:46 +0300

    chutkaTudaCuda

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // const now = new Date()
 
+let mode = 'time'
 const outputTitle = document.getElementById('output')
 const FullBtn = document.getElementById('full')
 const DateBtn = document.getElementById('date')
@@ -8,6 +9,21 @@ const TimeBtn = document.getElementById('time')
 outputTitle.textContent = new Date()
 
 setInterval(() => {
-	outputTitle.textContent = new Date()
+	outputTitle.textContent = formate(mode)
 }, 1000)
 //5.06.44
+
+const formate = formateMode => {
+	const now = new Date()
+
+	switch (formateMode) {
+		case 'time':
+			return now.toLocaleTimeString()
+		case 'date':
+			return now.toLocaleDateString()
+		case 'full':
+			return now.toLocaleDateString() + ' ' + now.toLocaleTimeString()
+		default:
+			return now.toLocaleDateString()
+	}
+}
